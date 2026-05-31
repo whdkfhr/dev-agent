4fb59563f959b5de11aadb1e317f17338b1bba5c
whdkfhr <whwoals18@navaer.com>
2026-05-31 14:56:28 +0900

    initialize spring boot project

---
+++ src/main/java/com/arok2/dev_agent/DevAgentApplication.java
@@ -0,0 +1,13 @@
+package com.arok2.dev_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAgentApplication.class, args);
+	}
+
+}
